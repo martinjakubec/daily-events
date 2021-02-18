9ada567370ed1e21efeef17a753a5db9d6168d25
martinjakubec <martin.kubo8@gmail.com>
2021-02-18 23:33:38 +0100

    fix styles enqueue

--- public/src/main.js
+++ public/src/main.js
@@ -1,6 +1,6 @@
+import './assets/scss/style.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/scss/style.scss'
 
 createApp(App).use(router).mount('#app')
