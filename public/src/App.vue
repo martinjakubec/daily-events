1322595a7b296584633a23deb10d800df4269299
martinjakubec <martin.kubo8@gmail.com>
2021-02-18 23:34:26 +0100

    modify main app component

--- public/src/App.vue
+++ public/src/App.vue
@@ -1,11 +1,97 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> 
-    <!-- <router-link to="/about">About</router-link> -->
-  </div>
-  <router-view/>
+  <the-header />
+  <the-navbar @userLogout="handleUserLogout" :isUserLoggedIn="isUserLoggedIn" :username="username" />
+  <router-view
+    @userLogin="handleUserLogin"
+    @userRegister="handleUserRegistration"
+    :isUserLoggedIn="isUserLoggedIn"
+    :userData="userData"
+  />
+  <base-error v-if="errorMessage" :errorMessage="errorMessage" @closeError="errorMessage=null" />
 </template>
 
-<style lang="scss">
+<script>
+import BaseError from './components/BaseError.vue';
+import TheHeader from './components/TheHeader.vue';
+import TheNavbar from './components/TheNavbar.vue';
+export default {
+  components: {TheHeader, TheNavbar, BaseError},
+  data() {
+    return {
+      isUserLoggedIn: false,
+      userData: null,
+      errorMessage: null,
+    };
+  },
+  computed: {
+    username() {
+      if(this.userData) {
+        return this.userData.username;
+      } else {
+        return 'usernameUnknown'
+      }
+    }
+  },
+  methods: {
+    async handleUserLogin(e) {
+      const username = e.target.username.value.trim();
+      const password = e.target.password.value.trim();
+      if (username && password) {
+        const body = JSON.stringify({username, password})
+        const userLogin = await fetch(process.env.VUE_APP_API_BASE_URL + 'login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: body
+        });
+        const userLoginResponse = await userLogin.json();
+        if (userLoginResponse.status === 'ok') {
+          this.isUserLoggedIn = true;
+          this.errorMessage = null;
+          localStorage.setItem('token', userLoginResponse.data);
+          this.$router.push('/')
+        } else {
+          this.errorMessage = userLoginResponse.error;
+        }
+      } else {
+        this.errorMessage = 'Please fill in both fields.'
+      }
+    },
+    handleUserLogout() {
+      this.isUserLoggedIn = false;
+      localStorage.removeItem('token');
+      this.$router.push('/login')
+    },
+    async handleUserRegistration() {
+      console.log('User should be registered');
+    },
+  },
+  async created() {
+    const token = localStorage.getItem('token');
+    if(token) {
+      const verifyToken = await fetch(process.env.VUE_APP_API_BASE_URL + 'validateToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({token})
+      })
+      const verifyTokenResponse = await verifyToken.json();
+      if (verifyTokenResponse.status === 'ok') {
+        this.isUserLoggedIn = true;
+      } else {
+        this.errorMessage = verifyTokenResponse.error;
+      }
+    }
+  }
+};
+</script>
 
+<style lang="scss">
+:root {
+  font-family: 'Roboto';
+}
 </style>
